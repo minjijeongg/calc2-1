ed49021d18a0509cbce7ea0441ea9a8adf398e7c
minjijeongg <mj323@njit.edu>
2021-11-04 01:36:24 -0400

    Assignment 1

--- calculator/operations/subtraction.py
+++ calculator/operations/subtraction.py
@@ -1,7 +1,8 @@
 """Subtraction Class"""
 from calculator.operations.calculation import Calculation
 
-class Subtraction(Calculation): #pylint: disable=too-few-public-methods
+
+class Subtraction(Calculation):  # pylint: disable=too-few-public-methods
     """Subtraction class"""
 
     def get_result(self):
